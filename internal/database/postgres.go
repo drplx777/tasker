26925ebda418e3d0c45160445ba67ff03141b5f1
drplx <drplx>
2025-08-12 18:19:26 +0700

    123

--- internal/database/postgres.go
+++ internal/database/postgres.go
@@ -55,40 +55,46 @@ func NewPool(ctx context.Context, connString string) (*pgxpool.Pool, error) {
 
 func createTables(ctx context.Context, pool *pgxpool.Pool) error {
 	query := `
-		CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-		
-		-- Создание таблицы Users
-		CREATE TABLE IF NOT EXISTS users (
-			id SERIAL PRIMARY KEY,
-			name TEXT NOT NULL,
-			surname TEXT NOT NULL,
-			middlename TEXT,
-			login TEXT NOT NULL UNIQUE,
-			roleID INTEGER NOT NULL,
-			password TEXT NOT NULL
-		);
-		
-		-- Создание таблицы Tasks
-		CREATE TABLE IF NOT EXISTS tasks (
-			id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-			title TEXT NOT NULL,
-			description TEXT NOT NULL,
-			status TEXT NOT NULL,
-			"reporterD" TEXT NOT NULL,
-			"assignerID" TEXT,
-			"reviewerID" TEXT,
-			"approverID" TEXT NOT NULL,
-			"approveStatus" TEXT NOT NULL,
-			created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-			updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-			"started_At" TIMESTAMPTZ,
-			done_at TIMESTAMPTZ,
-			deadline TEXT NOT NULL,
-			"dashboardID" TEXT NOT NULL,
-			"blockedBy" TEXT[]
-		);
-	`
+        CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-	_, err := pool.Exec(ctx, query)
-	return err
+        CREATE TABLE IF NOT EXISTS users (
+            id SERIAL PRIMARY KEY,
+            name TEXT NOT NULL,
+            surname TEXT NOT NULL,
+            middlename TEXT,
+            login TEXT NOT NULL UNIQUE,
+            roleID INTEGER NOT NULL,
+            password TEXT NOT NULL
+        );
+
+        CREATE TABLE IF NOT EXISTS tasks (
+            id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+            title TEXT NOT NULL,
+            description TEXT NOT NULL,
+            status TEXT NOT NULL,
+            "reporterD" TEXT NOT NULL,
+            "assignerID" TEXT,
+            "reviewerID" TEXT,
+            "approverID" TEXT NOT NULL,
+            "approveStatus" TEXT NOT NULL,
+            created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+            updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+            "started_At" TIMESTAMPTZ,
+            done_at TIMESTAMPTZ,
+            deadline TEXT NOT NULL,
+            "dashboardID" TEXT NOT NULL,
+            "blockedBy" TEXT[]
+        );
+    `
+
+	if _, err := pool.Exec(ctx, query); err != nil {
+		return err
+	}
+
+	// Добавляем новое поле, если его ещё нет
+	if _, err := pool.Exec(ctx, `ALTER TABLE tasks ADD COLUMN IF NOT EXISTS space TEXT;`); err != nil {
+		return err
+	}
+
+	return nil
 }
